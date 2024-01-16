13446778dd6a497d463d6b29be39d4a840addcc8
Thor Anderson <ta.anderson@gmail.com>
2024-01-16 11:05:30 -0700

    Got Vite configured to compile TypeScript

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+// basic data types of TypeScript
+/* TypeScript adds several data types on top of JavaScript's data types:
+- boolean
+- number
+- string
+- array
+- tuple
+- enum
+- any
+- void
+- null and undefined
+- never
+- object 
+*/
+
+// boolean
+let isDone: boolean = false;
+console.log(isDone);
