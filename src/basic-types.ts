79111d84f32d7829fa57089c4075b83274df9b69
Thor Anderson <ta.anderson@gmail.com>
2024-01-25 11:11:57 -0700

    Refactored and added array examples

--- src/basic-types.ts
+++ src/basic-types.ts
@@ -13,18 +13,40 @@ export function basicTypesExample() {
         - null and undefined
         - never
         - object `,
-    code: `// boolean
-let isDone: boolean = false; // "is assigned the value of"
+    code: `
+// boolean
+    let isDone: boolean = false; // "is assigned the value of"
 // isDone is a variable annotated with the boolean data type and assigned the value false.
 
 // number
-let myNumber: number = 42;
+    let myNumber: number = 42;
 // myNumber is a variable annotated with the number data type and assigned the value 42.
 
 // string
-let myName: string = "Thor";
+    let myName: string = "Thor";
 // myName is a variable annotated with the string data type and assigned the value "Thor".
 `,
+  }
+}
+
+export function arrayTypesExample() {
+  return {
+    title: "Array Types",
+    explanation: `TypeScript adds a few different types for arrays, including: array, tuple, and enum.`,
+    code: `
+    // Array
+    let myArray: number[] = [1, 2, 3, 4, 5];
+    let myStringArray: string[] = ["Hello", "World"];
+
+    // Tuple
+    let myTuple: [string, number] = ["Hello", 42];
+    // Tuples are fixed-length arrays where each element has a specific data type
+    // In TypeScript, you can specify any number of elements in a tuple, but the data types of each element must match the data types specified in the tuple declaration.
+
+    // Enum
+    enum Color {Red, Green, Blue};
+    // Enums are a way of giving more friendly names to sets of numeric values.
+    `
   }
 }
 
@@ -42,4 +64,16 @@ export function functionTypesExample() {
 
 function add(x: number, y: number): number {
     return x + y;
+}
+
+let myArray: number[] = [1, 2, 3, 4, 5];
+let myStringArray: string[] = ["Hello", "World"];
+
+enum Color {Red, Island_Teal, Bahama_Green, Quartz}
+
+const sportsCar = {
+    make: "Porsche",
+    model: "911",
+    year: 2019,
+    color: Color.Bahama_Green
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,49 @@
-import { basicTypesExample } from "./basic-types";
+import { basicTypesExample, arrayTypesExample } from "./basic-types";
 
-document.querySelector("#basicTypesButton").addEventListener("click", () => {
+/* document.querySelector("#basicTypesButton").addEventListener("click", () => {
     const basicTypes = basicTypesExample();
     document.querySelector("#basicTypesTitle").innerHTML = basicTypes.title;
     document.querySelector("#basicTypesExplanation").innerHTML = basicTypes.explanation;
     document.querySelector("#basicTypesCode").innerHTML = basicTypes.code;
+    document.querySelector("#codeDiv").classList.add("mockup-code");
 })
+
+document.querySelector("#arrayTypesButton").addEventListener("click", () => {
+    const arrayTypes = arrayTypesExample();
+    document.querySelector("#basicTypesTitle").innerHTML = arrayTypes.title;
+    document.querySelector("#basicTypesExplanation").innerHTML = arrayTypes.explanation;
+    document.querySelector("#basicTypesCode").innerHTML = arrayTypes.code;
+    document.querySelector("#codeDiv").classList.add("mockup-code");
+}) */
+
+document.querySelector("#basicTypesButton").addEventListener("click", () => loadExample("basic"))
+document.querySelector("#arrayTypesButton").addEventListener("click", () => loadExample("array"))
+
+const cardTitle = document.querySelector("#cardTitle");
+const cardExplanation = document.querySelector("#cardExplanation");
+const cardCode = document.querySelector("#cardCode");
+
+function loadExample(exampleType: string) {
+    // based on the exampleType, load the appropriate example
+    let chosenExampleType
+    switch (exampleType) {
+        case "basic":
+            chosenExampleType = basicTypesExample();
+            break;
+        case "array":
+            chosenExampleType = arrayTypesExample();
+            break;
+        default:
+            chosenExampleType = basicTypesExample();
+    }
+
+    cardTitle.textContent = chosenExampleType.title;
+    cardExplanation.textContent = chosenExampleType.explanation;
+    cardCode.innerHTML = chosenExampleType.code;
+    document.querySelector("#codeDiv").classList.add("mockup-code");
+}
+/*
+Don't
+Repeat
+Yourself
+*/
