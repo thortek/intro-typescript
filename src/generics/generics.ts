c60306e5b3d8cb724be9ab0bf964b16fb063991f
Thor Anderson <ta.anderson@gmail.com>
2024-04-02 11:12:17 -0600

    Improved detail and list views - more to come

--- src/generics/generics.ts
+++ src/generics/generics.ts
@@ -20,12 +20,48 @@ const booleanArray = [true, true, false, false];
 const reversedBooleanArray = reverse(booleanArray);
 console.log(reversedBooleanArray);
 
-const exampleOne = new ExampleCodeSnippet('Generic Function Example', 'This is an example of a generic function that reverses an array of any type.', `function reverse<T>(itemArray: T[]): T[] {
+const detailView = document.querySelector('#detail-view');
+const detailViewExample = document.createElement('div');
+detailViewExample.classList.add('text-3xl', 'w-full', 'p-4', 'mx-4', 'bg-[#333333DD]')
+detailView.appendChild(detailViewExample);
+
+const exampleOne = new ExampleCodeSnippet('Generic Function Example', 
+'This is an example of a generic function that reverses an array of any type.',
+`function reverse<T>(itemArray: T[]): T[] {
     return itemArray.reverse();
 }`);
 
+const exampleTwo = new ExampleCodeSnippet('Generic Classes',
+'Generic classes allow you to write code that can be reused with different types of data.',
+`class DataStorage<T> {
+    private data: T
+
+    constructor(data: T) {
+        this.data = data;
+    }
+
+    getData(): T {
+        return this.data;
+    }
+}`)
+
 const listView = document.querySelector('#list-view');
-listView.innerHTML = exampleOne.renderCodeSnippet();
+const exampleOneElement = document.createElement('div');
+exampleOneElement.classList.add('text-[.5rem]', 'bg-slate-900', 'p-1', 'm-[2px]', 'rounded', 'overflow-x-auto', 'hover:bg-slate-700', 'hover:cursor-pointer')
+
+exampleOneElement.innerHTML = exampleOne.renderCodeSnippet();
+listView.appendChild(exampleOneElement)
+
+exampleOneElement.addEventListener('click', () => {
+    detailViewExample.innerHTML = exampleOne.renderCodeSnippet();
+})
+
+
+const exampleTwoElement = document.createElement('div');
+exampleTwoElement.classList.add('text-[.5rem]', 'bg-slate-900', 'p-1', 'm-[2px]', 'rounded', 'overflow-x-auto', 'hover:bg-slate-700', 'hover:cursor-pointer')
+
+exampleTwoElement.innerHTML = exampleTwo.renderCodeSnippet();
+listView.appendChild(exampleTwoElement)
 
 
 /* Generics in TypeScript are a tool that allows you to create reusable code components that can work over a variety of types rather than a single one. This allows users to consume these components and use their own types.
@@ -37,3 +73,25 @@ function identity<T>(arg: T): T {
 }
 
 In this example, T is a type variable—a kind of variable that works on types rather than values. This function will return whatever is sent into it, and the type T will be determined by the caller of the function when they pass an argument. */
+
+/* Generic classes allow you to write code that can be reused with different types of data.
+
+class DataStorage<T> {
+    private data: T
+
+    constructor(data: T) {
+        this.data = data;
+    }
+
+    getData(): T {
+        return this.data;
+    }
+}
+
+const numberStorage = new DataStorage<number>(10);
+const stringStorage = new DataStorage<string>('Hello');
+const booleanStorage = new DataStorage<boolean>(true);
+const objectStorage = new DataStorage<object>({name: 'Frankenstein'});
+const arrayStorage = new DataStorage<number[]>([1, 2, 3, 4, 5]);
+
+*/
--- src/lib/classes/ExampleCodeSnippet.ts
+++ src/lib/classes/ExampleCodeSnippet.ts
@@ -14,7 +14,7 @@ export class ExampleCodeSnippet implements ExampleCode {
     return `<h3 class="font-bold text-black bg-slate-400 rounded p-1 my-4 text-center">
     ${this.title}
     </h3>
-    <p class="text-white">${this.explanation}</p>
+    <p class="text-white mb-1">${this.explanation}</p>
     <pre>
     <code>${hljs.highlight(this.code, { language: "typescript" }).value}</code>
     </pre>`
